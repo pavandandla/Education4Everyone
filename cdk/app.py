65473a622298c622e1e9bd3eeb81ec4e9cb0565d
pavandandla <pavan.kumar@aithinkers.com>
2025-03-26 18:54:17 +0530

    Setup3

--- cdk/app.py
+++ cdk/app.py
@@ -1,5 +1,5 @@
 from aws_cdk import App
-from ec2_stack import EC2Stack  # Ensure this module exists and is accessible
+from lib.ec2_stack import EC2Stack  # Ensure this module exists and is accessible
 
 app = App()
 EC2Stack(app, "EC2Stack")  # Instantiate EC2Stack with app and ID
